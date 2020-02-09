8a54a83d56da3129af21bd953d5327abea359e6a
Antti Simonen <antti.simonen@gofore.com>
2020-02-09 11:35:48 +0200

    feat: added version endpoint

---
+++ src/LetkaBike/LetkaBike.API/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetkaBike.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/1.0/[controller]")]
+    [ApiController]
+    public class VersionController : Controller
+    {
+        public IActionResult Get()
+        {
+            return new JsonResult(Assembly.GetEntryAssembly()?.GetName().Version);
+        }
+    }
+}
